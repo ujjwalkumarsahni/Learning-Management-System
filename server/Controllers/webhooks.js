f2beab8b6f2f11a9e322f0454e10e8e93901b42d
Ujjwal Kumar <133194307+ujjwalkumarsahni@users.noreply.github.com>
2025-02-15 18:50:50 +0530

    stripe payment int..

--- server/Controllers/webhooks.js
+++ server/Controllers/webhooks.js
@@ -1,6 +1,8 @@
 import { Webhook } from "svix";
 import User from "../Models/User.js";
 import Stripe from "stripe";
+import Purchase from './../Models/Purchase.js';
+import Course from "../Models/Course.js";
 
 
 // Function to manage Clerk User with Database
@@ -42,15 +44,76 @@ export const clerkWebhooks = async (req, res) => {
                 return res.json({ success: true });
             }
             default:
-                return res.status(400).json({ success: false, message: "Unhandled event type" });
+                return res
+                    .status(400)
+                    .json({ success: false, message: "Unhandled event type" });
         }
     } catch (error) {
         return res.status(500).json({ success: false, message: error.message });
     }
 };
 
-const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY)
+const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-export const stripeWebhooks = async (request,response) =>{
+export const stripeWebhooks = async (request, response) => {
+    const sig = request.headers['stripe-signature'];
 
-}
+    let event;
+
+    try {
+        event = stripe.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+    }
+    catch (err) {
+        response.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    // Handle the event
+    switch (event.type) {
+        case "payment_intent.succeeded": {
+            const paymentIntent = event.data.object;
+            const paymentIntentId = paymentIntent.id
+
+            const session = await stripeInstance.checkout.session.list({
+                payment_intent: paymentIntentId
+            })
+            const { purchaseId } = session.data[0].metadata
+
+            const purchaseData = await Purchase.findById(purchaseId)
+            const userData = await User.findById(purchaseId.userId)
+            const courseData = await Course.findById(purchaseId.courseId.toString())
+
+            courseData.enrolledStudents.push(userData)
+            await courseData.save()
+
+            userData.enrolledCourses.push(courseData._id)
+            await userData.save()
+
+            purchaseData.status = 'completed'
+            await purchaseData.save()
+
+            break;
+        }
+        case "payment_intent.payment_failed": {
+            const paymentIntent = event.data.object;
+            const paymentIntentId = paymentIntent.id
+
+            const session = await stripeInstance.checkout.session.list({
+                payment_intent: paymentIntentId
+            })
+            const { purchaseId } = session.data[0].metadata
+
+            const purchaseData = await Purchase.findById(purchaseId)
+
+            purchaseData.status = 'failed'
+            await purchaseData.save()
+
+            break;
+        }
+        // ... handle other event types
+        default:
+            console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a response to acknowledge receipt of the event
+    response.json({ received: true });
+};
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 
 import connectDB from './configs/mongodb.js'
-import { clerkWebhooks } from './Controllers/webhooks.js'
+import { clerkWebhooks, stripeWebhooks } from './Controllers/webhooks.js'
 import educatorRoute from './routes/educatorRoutes.js'
 import { clerkMiddleware } from '@clerk/express'
 import connectCloudinary from './configs/cloudinary.js';
@@ -30,6 +30,7 @@ app.post('/clerk', express.json(), clerkWebhooks)
 app.use('/api/educator', express.json(), educatorRoute)
 app.use('/api/course', express.json(), courseRouter)
 app.use('/api/user', express.json(), userRouter)
+app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
